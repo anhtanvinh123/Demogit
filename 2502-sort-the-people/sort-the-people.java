a3a4a63c9e22d8f81f449f73b514ba469610ae11
anhtanvinh123 <anhtanvinh123@gmail.com>
2026-05-14 22:30:58 +0700

    Time: 9 ms (81.80%) | Memory: 47.1 MB (77.27%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,22 +1,23 @@
+import java.util.*;
+
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        // Sắp xếp giảm dần theo heights (Bubble Sort đơn giản)
-        for (int i = 0; i < heights.length - 1; i++) {
-            for (int j = 0; j < heights.length - i - 1; j++) {
-                if (heights[j] < heights[j + 1]) {
-                    // đổi heights
-                    int tempHeight = heights[j];
-                    heights[j] = heights[j + 1];
-                    heights[j + 1] = tempHeight;
+        int n = names.length;
 
-                    // đổi names tương ứng
-                    String tempName = names[j];
-                    names[j] = names[j + 1];
-                    names[j + 1] = tempName;
-                }
-            }
+        List<int[]> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(new int[]{heights[i], i});
         }
 
-        return names;
+        list.sort((a, b) -> b[0] - a[0]); // sort giảm dần theo height
+
+        String[] res = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            res[i] = names[list.get(i)[1]];
+        }
+
+        return res;
     }
 }
