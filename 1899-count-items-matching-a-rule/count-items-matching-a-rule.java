e2c32b3bfecb322beb2e0fd648d07c42dc64c90a
anhtanvinh123 <anhtanvinh123@gmail.com>
2026-05-14 22:07:44 +0700

    Time: 4 ms (96.34%) | Memory: 49.5 MB (82.50%) - LeetSync

---
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = 0;
+
+        // Xác định vị trí cần so sánh
+        if (ruleKey.equals("type")) {
+            index = 0;
+        } else if (ruleKey.equals("color")) {
+            index = 1;
+        } else {
+            index = 2;
+        }
+
+        int count = 0;
+
+        // Duyệt từng item
+        for (List<String> item : items) {
+            if (item.get(index).equals(ruleValue)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
